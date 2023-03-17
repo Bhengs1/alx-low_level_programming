368995a2f8782884eb6b3198bebb48d27ff3bcd3
Bhengs1 <sphesihlebhengs@gmail.com>
2023-03-16 20:34:01 -0700

    5a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+
 /**
- * main - with proper grammar,
- * but the outcome in a piece of art
+ * main - Hello Printf
  *
  * Return: 0 (Success)
  */
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - size of various types
+ * Return 0 (Success)
+ * */
+int main(void)
+{
+	char 
