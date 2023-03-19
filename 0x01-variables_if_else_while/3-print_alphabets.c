237c3353e3ab9597c7430e82c3757c8acf77416c
Bhengs1 <sphesihlebhengs@gmail.com>
2023-03-19 07:03:01 -0700

    print alphabet 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - different caps alphabets
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
